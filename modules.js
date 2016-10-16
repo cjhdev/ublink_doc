e9414a3b994faa48fda2ac28a7206d18f9465691
cjhdev <cam@cjh.id.au>
2016-10-16 02:26:34 +0100

    doxygen generated from master ae2411afe71a1b1bb00edd0efaf87dfceca868b5

--- modules.js
+++ modules.js
@@ -1,6 +1,7 @@
 var modules =
 [
     [ "Compact form", "group__blink__compact__form.html", "group__blink__compact__form" ],
+    [ "Event Driven Decoder", "group__blink__decoder.html", "group__blink__decoder" ],
     [ "Parser", "group__blink__parser.html", "group__blink__parser" ],
     [ "Lexer", "group__blink__lexer.html", "group__blink__lexer" ]
 ];
