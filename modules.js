fda1a85718d53c8f8ae0b8f6c4a4e77c177a48cb
cjhdev <cam@cjh.id.au>
2016-11-06 13:00:37 +0000

    doxygen generated from master 075ddeec5ecf0d4e9f5b341b39fe4605d071ff0e

--- modules.js
+++ modules.js
@@ -1,7 +1,7 @@
 var modules =
 [
-    [ "Compact Form", "group__blink__compact__form.html", "group__blink__compact__form" ],
-    [ "Event Driven Decoder", "group__blink__decoder.html", "group__blink__decoder" ],
-    [ "Parser", "group__blink__parser.html", "group__blink__parser" ],
-    [ "Lexer", "group__blink__lexer.html", "group__blink__lexer" ]
+    [ "Blink_compact_form", "group__blink__compact__form.html", "group__blink__compact__form" ],
+    [ "Blink_event_decoder", "group__blink__event__decoder.html", "group__blink__event__decoder" ],
+    [ "Blink_parser", "group__blink__parser.html", "group__blink__parser" ],
+    [ "Blink_lexer", "group__blink__lexer.html", "group__blink__lexer" ]
 ];
